39f49c22879b7e7886ae4c5959607fe7d92f85e7
Zach Boemer <49701027+zachboemer@users.noreply.github.com>
2023-01-24 14:37:22 -0600

    api request for /pint-of-day

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = 'https://news-pint-backend.herokuapp.com/';
 
-export const getArticles = async () => {
+export const getAllArticles = async () => {
   try{
       const response = await axios.get(`${baseURL}articles`);
       return response.data;
@@ -10,3 +10,21 @@ export const getArticles = async () => {
       throw error;
   }
 };
+
+export const getPintOfDay = async () => {
+  try{
+      const response = await axios.get(`${baseURL}pint-of-day/`);
+      return response.data;
+  }catch(error){
+      throw error;
+  }
+}
+
+// export const getPintOfDay = async (date) => {
+//   try{
+//       const response = await axios.get(`${baseURL}pint-of-day/${date}`);
+//       return response.data;
+//   }catch(error){
+//       throw error;
+//   }
+// }
